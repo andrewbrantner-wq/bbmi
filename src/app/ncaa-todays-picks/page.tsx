f14bc548c682e2d95d71cc965e96c458edd257be
Andrew Brantner <andrewbrantner@gmail.com>
2026-03-11 22:59:40 -0500

    Auto-deploy 2026-11-Wed 225940.769368

--- src/app/ncaa-todays-picks/page.tsx
+++ src/app/ncaa-todays-picks/page.tsx
@@ -509,7 +509,7 @@ export default function BettingLinesPage() {
    /* <AuthProvider>
       <ProtectedRoute>*/
         <BettingLinesPageContent />
-   /*   </ProtectedRoute>
+   /*   </ProtectedRoute> 
     </AuthProvider>*/
   );
 }
